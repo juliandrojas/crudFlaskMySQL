4ff2083f81d33db1640f0cc019c8cba0a75afab2
Julian D Rojas <juliandra140201@gmail.com>
2022-08-27 15:39:55 -0500

    Creation of routes and functions, the latter with initial messages.

--- Controller/App.py
+++ Controller/App.py
@@ -4,9 +4,29 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 #Cambiamos ruta para evitar que el servidor arroje un Error 404
 @app.route('/')
-#Creamos una función para manejar las rutas
+#Creamos una función para manejar la ruta
 def Index():
-    print("Principal Route")
+    return "Principal Route"
+#Ruta para añadir clientes
+@app.route('/addClient')
+#Creamos una función para manejar la ruta
+def addClient():
+    return "Add Client Route"
+#Ruta para listar clientes
+@app.route('/listClients')
+#Creamos una función para manejar la ruta
+def listClients():
+    return "List Clients Route"
+#Ruta para actualizar clientes
+@app.route('/updateClient')
+#Creamos una función para manejar la ruta
+def updateClients():
+    return "Update Clients Route"
+#Ruta para eliminar clientes
+@app.route('/deleteClient')
+#Creamos una función para manejar la ruta
+def deleteClient():
+    return "Delete Clients Route"
 #Comprobamos si el archivo que ese está ejecutando es el principal
 if __name__ == '__main__':
     '''
