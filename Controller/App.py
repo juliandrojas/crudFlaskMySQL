9d284ad91d209fbb434fe74f59d10b7f8d80dd38
Julian D Rojas <juliandra140201@gmail.com>
2022-08-27 15:33:54 -0500

    Initial route management

--- Controller/App.py
+++ Controller/App.py
@@ -2,6 +2,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 #Configuramos la conexión
 app = Flask(__name__)
+#Cambiamos ruta para evitar que el servidor arroje un Error 404
+@app.route('/')
+#Creamos una función para manejar las rutas
+def Index():
+    print("Principal Route")
 #Comprobamos si el archivo que ese está ejecutando es el principal
 if __name__ == '__main__':
     '''
