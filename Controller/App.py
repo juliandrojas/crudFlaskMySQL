b27fea780bec4dcc6e7394ffce90bd3e1dc5667d
Julian D Rojas <juliandra140201@gmail.com>
2022-08-27 15:29:07 -0500

    Initial configuration: Port and debug in true

--- Controller/App.py
+++ Controller/App.py
@@ -1,2 +1,9 @@
 #Llamamos al framework de Flask y las funciones render_template, request, redirect, url_for y fkas
 from flask import Flask, render_template, request, redirect, url_for, flash
+#Comprobamos si el archivo que ese está ejecutando es el principal
+if __name__ == '__main__':
+    '''
+    Asignamos puertos y para reiniciar constantemente el servidor
+    cada vez que guardemos cambios usamos el parámetro debug=True
+    '''
+    app.run(port=3000, debug=True)
