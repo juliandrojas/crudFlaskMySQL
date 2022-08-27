c58ea9d989db8184d9c296f995faa774f39b4b77
Julian D Rojas <juliandra140201@gmail.com>
2022-08-27 15:31:01 -0500

    Configurating the connection

--- Controller/App.py
+++ Controller/App.py
@@ -1,5 +1,7 @@
 #Llamamos al framework de Flask y las funciones render_template, request, redirect, url_for y fkas
 from flask import Flask, render_template, request, redirect, url_for, flash
+#Configuramos la conexión
+app = Flask(__name__)
 #Comprobamos si el archivo que ese está ejecutando es el principal
 if __name__ == '__main__':
     '''
